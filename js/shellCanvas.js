1dcfde7affec511b44924b63122977efa3beab94
DrFreudJr <drfreudj88@gmail.com>
2021-01-27 18:11:55 -0500

    Improved canvas resize

--- js/shellCanvas.js
+++ js/shellCanvas.js
@@ -2,35 +2,53 @@
 // 'use strict';
 let cl = console.log;
 
-window.onload = function () {
+window.onload = function () {   // onload wrapper
 
-elContext = document.querySelector("canvas")
-context = elContext.getContext("2d");
+var canvas;    // Global canvas object reference
+var context;       // Global 2D context reference
 
-initialize()
-
-function initialize () {
-    window.addEventListener('resize', resizeCanvas, false);
-    resizeCanvas()
+function createCanvas () {                
+    const canvas = document.createElement("canvas"); 
+    canvas.style.position = "absolute"; 
+    canvas.style.left     = "0px";      
+    canvas.style.top      = "0px";
+    // canvas.style.zIndex   = 1;        
+    document.body.appendChild(canvas);  // Add to document
+    return canvas;
 }
 
-function resizeCanvas() {
-    elContext.width = window.innerWidth;
-    elContext.height = window.innerHeight;
-    drawScreen();
+function sizeCanvas () {                // Create or resize 
+    if (canvas === undefined) {         // Check for global canvas reference
+        canvas = createCanvas();        // Create a new canvas element
+        context = canvas.getContext("2d");  // Get the 2D context
+    }
+    canvas.width  = innerWidth;         // Set the canvas resolution to fill the page
+    canvas.height = innerHeight;   
+    drawScreen()     
 }
+
+addEventListener("resize", sizeCanvas); // Event listener
+
+sizeCanvas();   // Begin the resize loop by creating canvas
+
+// // // // // // // // // // // Evrything above here is BP responsive full-screen canvas
 
 function drawScreen() {
-    var c = new Image()
+
+    let width = innerWidth
+    let height = innerHeight
+    let backgroundColor = 'black'
+
+    context.fillStyle = backgroundColor             // set background color
+    context.fillRect(0, 0, width, height)
+
+    const c = new Image()                   // draw background image
     c.src = "bostonSat.webp"   
     context.drawImage(c, 0, 0)
 
-
-
 }
 
 
 
 
-
-}   //end onload wrapper
+}   //onload wrapper
