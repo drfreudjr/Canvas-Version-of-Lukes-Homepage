da68792bac20c87bd1796bcc9acd10d826567eb7
DrFreudJr <drfreudj88@gmail.com>
2021-01-27 13:41:06 -0500

    Initial commit - just the background

---
+++ js/shellCanvas.js
@@ -0,0 +1,36 @@
+/* Javascript Shell */
+// 'use strict';
+let cl = console.log;
+
+window.onload = function () {
+
+elContext = document.querySelector("canvas")
+context = elContext.getContext("2d");
+
+initialize()
+
+function initialize () {
+    window.addEventListener('resize', resizeCanvas, false);
+    resizeCanvas()
+}
+
+function resizeCanvas() {
+    elContext.width = window.innerWidth;
+    elContext.height = window.innerHeight;
+    drawScreen();
+}
+
+function drawScreen() {
+    var c = new Image()
+    c.src = "bostonSat.webp"   
+    context.drawImage(c, 0, 0)
+
+
+
+}
+
+
+
+
+
+}   //end onload wrapper
